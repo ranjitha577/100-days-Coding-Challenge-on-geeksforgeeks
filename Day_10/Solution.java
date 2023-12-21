6f4a508d03141aaf5fa0334face9f5bbdaa663d8
ranjitha5777 <154562523+ranjitha5777@users.noreply.github.com>
2023-12-22 04:22:15 +0530

    Add files via upload

--- Day_10/Solution.java
+++ Day_10/Solution.java
@@ -1,1 +1,29 @@
+Question:- Given an array Arr of N positive integers. Your task is to find the elements whose value is equal to that of its index value ( Consider 1-based indexing ).
 
+Example 1:-
+
+Input:
+N = 5
+Arr[] = {15, 2, 45, 12, 7}
+Output: 2
+
+Example 2:-
+
+Input: 
+N = 1
+Arr[] = {1}
+Output: 1
+
+time complexity is O(N)
+
+class Solution {
+    ArrayList<Integer> valueEqualToIndex(int arr[], int n) {
+        ArrayList<Integer> Arr = new ArrayList<>();
+        for (int i=0; i<arr.length; i++) {
+            if (i+1 == arr[i]){
+                Arr.add(i+1);
+            }
+        }
+        return Arr;
+    }
+}
