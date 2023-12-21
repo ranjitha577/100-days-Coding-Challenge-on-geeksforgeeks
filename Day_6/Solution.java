a1c46433ecc5ffbee817ff7c381a74811bfb13c2
ranjitha5777 <154562523+ranjitha5777@users.noreply.github.com>
2023-12-22 04:19:10 +0530

    Add files via upload

--- Day_6/Solution.java
+++ Day_6/Solution.java
@@ -1,1 +1,27 @@
+Question:- You are given a string s, you need to reverse the string.
 
+Example 1:
+
+Input:
+s = Geeks
+Output: skeeG
+Example 2:
+
+Input:
+s = for
+Output: rof
+
+time complexity is O(N)
+
+class Reverse
+{
+   public static String reverseWord(String str)
+    {
+        // Reverse the string str
+        String s="";
+        for(int i=str.length()-1;i>=0;i--){
+           s=s+str.charAt(i);
+        }
+       return s;
+    }
+}
