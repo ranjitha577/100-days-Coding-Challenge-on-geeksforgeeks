8b3daf6235df839353d91406a60d74db8f2566d0
ranjitha5777 <154562523+ranjitha5777@users.noreply.github.com>
2023-12-22 04:27:02 +0530

    Add files via upload

--- Day_14/Solution.java
+++ Day_14/Solution.java
@@ -1,1 +1,33 @@
+Question:- Given two integers L, R, and digit X. Find the number of occurrences of X in all the numbers in the range (L, R) excluding L and R.
 
+Example 1:-
+
+Input:
+L=10, R=19, X=1
+Output: 9
+
+Example 2:-
+
+Input:
+L=18, R=81, X=9
+Output: 7
+
+time complexity is O(N)
+
+class Solution {
+    int countX(int l, int r, int x) {
+        int count = 0;
+        
+        for(int i = l + 1; i < r; i++) {
+            int num = i;
+            while(num > 0) {
+                if(num % 10 == x) 
+                    count++;
+                
+                num s/= 10;
+            }
+        }
+        
+        return count;
+    }
+}
