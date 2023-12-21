ae1e21c348c583d0a7dd17e73a92bd189fdbdcf1
ranjitha5777 <154562523+ranjitha5777@users.noreply.github.com>
2023-12-22 04:30:20 +0530

    Add files via upload

--- Day_17/Solution.java
+++ Day_17/Solution.java
@@ -1,1 +1,58 @@
+Question:-Given two BSTs containing N1 and N2 distinct nodes respectively and given a value x, your task is to complete the function countPairs(), that returns the count of all pairs of (a, b), where a belongs to one BST and b belongs to another BST, such that a + b = x.
+
+Example 1:-
+
+Input:
+BST1:
+       5
+     /   \
+    3     7
+   / \   / \
+  2   4 6   8
+BST2:
+       10
+     /    \
+    6      15
+   / \    /  \
+  3   8  11   18
+x = 16
+Output: 3
+
+Example 2:-
+
+Input:
+BST1:
+  1
+   \
+    3
+   /
+  2
+BST2:
+    3
+   / \
+  2   4
+ /     
+1
+x = 4
+Output: 3
+
+time complexity is O(N)
+
+class Solution
+{
+    private static int traverse(Node node,int k){
+        if(node == null) return 0;
+        if(node.data == k) {
+            return 1;
+        }
+        else if(node.data > k) return traverse(node.left,k);
+        else return traverse(node.right,k);
+    }
+    public static int countPairs(Node root1, Node root2, int x)
+    {
+        if(root1 == null) return 0;
+        return traverse(root2,x-root1.data) + countPairs(root1.left,root2,x) + countPairs(root1.right,root2,x);
+        // Code here
+    }
+}
 
