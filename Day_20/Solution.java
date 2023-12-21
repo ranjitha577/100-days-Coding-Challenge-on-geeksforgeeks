34c0f11b909e28fd34d989fc37acbcd936485f46
ranjitha5777 <154562523+ranjitha5777@users.noreply.github.com>
2023-12-22 04:32:36 +0530

    Add files via upload

--- Day_20/Solution.java
+++ Day_20/Solution.java
@@ -1,1 +1,30 @@
+Question:-Consider a directed graph whose vertices are numbered from 1 to n. There is an edge from a vertex i to a vertex j if and only if either j = i + 1 or j = 3 * i. The task is to find the minimum number of edges in a path from vertex 1 to vertex n.
 
+Example 1:-
+
+Input:
+n = 9
+Output: 2
+
+Example 2:-
+
+Input:
+n = 4
+Output: 2
+
+time complexity is O(N)
+
+class Solution{
+    public int minStep(int n)
+    {
+        int c=0;
+        while(n!=1){
+            if(n%3==0) {n=n/3;}
+            else{
+            n--;}
+            c++;
+            
+        } return c;
+       
+    }
+}
